106ecb33a5f2e8078364f7e7adaa9f2fb0340912
Achile Egbunu <achilesolomon@gmail.com>
2023-07-01 18:43:25 -0500

    Add getPlayerChoice function
    This function enables a user to choose either rock, paper or scissors.
    Also, getComputerChoice was modified to return values in lowercase.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function getComputerChoice() {
     // generate random number between 0 and 3
     const num = Math.floor(Math.random() * 3);
 
-    return values[num];
+    return values[num].toLowerCase();
+}
+
+/** This function gets user selection of Rock, Paper, or Scissors */
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter rock, paper, or scissors", "paper");
+
+    while (playerChoice.toLowerCase() !== "rock" &&
+        playerChoice.toLowerCase() !== "paper" &&
+        playerChoice.toLowerCase() !== "scissors"
+    ) {
+        playerChoice = prompt("Enter rock, paper, or scissors", "paper");
+    }
+    return playerChoice;
 }
