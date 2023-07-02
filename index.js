5df4bf2697abde56f1380a603937eb6257a38171
Achile Egbunu <achilesolomon@gmail.com>
2023-07-01 19:11:30 -0500

    Add function playRound

--- index.js
+++ index.js
@@ -21,5 +21,31 @@ function getPlayerChoice() {
     ) {
         playerChoice = prompt("Enter rock, paper, or scissors", "paper");
     }
-    return playerChoice;
+    return playerChoice.toLowerCase();
+}
+
+/** This function plays a single round of rock, paper, and scissors game */
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a draw! You chose ${playerSelection} and computer chose ${computerSelection}.`;
+    }
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return `You lose! ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))} beats ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))}`
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))} beats ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))}`
+    }
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))} beats ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))}`
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `You lose! ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))} beats ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))}`
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `You lose! ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))} beats ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))}`
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))} beats ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))}`
+    }
 }
