74d341f79cdb503602ecc1d6ecae11f0ddff6916
Achile Egbunu <achilesolomon@gmail.com>
2023-07-01 20:06:20 -0500

    Add function to play game 5 times

--- index.js
+++ index.js
@@ -49,3 +49,45 @@ function playRound(playerSelection, computerSelection) {
         return `You Win! ${playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1))} beats ${computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1))}`
     }
 }
+
+/** This function plays 5 rounds of the game */
+
+function game() {
+
+    const NUMBER_OF_GAMES = 5;
+    let userScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+    let gameCount = 1;
+
+    while (gameCount <= NUMBER_OF_GAMES) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+
+        // play game
+        let gameMessage = playRound(playerSelection, computerSelection);
+
+        if (gameMessage.includes("You lose")) {
+            computerScore++;
+        } else if (gameMessage.includes("You Win!")) {
+            userScore++;
+        } else if (gameMessage.includes("draw!")) {
+            drawScore++;
+        }
+        gameCount++;
+    }
+
+    // display winner
+    return displayWinner(userScore, computerScore, drawScore);
+}
+
+/** Function to display game score and winner */
+function displayWinner(userScore, computerScore, drawScore) {
+    if (userScore > computerScore) {
+        return `You Win!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}.`;
+    } else if (computerScore > userScore) {
+        return `You Lose!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}.`;
+    } else {
+        return `Game is a draw!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}`;
+    }
+}
