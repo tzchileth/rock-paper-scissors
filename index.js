802a72a48182305cf089747d621c5120e92d1602
Achile Egbunu <achilesolomon@gmail.com>
2023-07-06 23:43:42 -0500

    Add eventlisteners for buttons

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 
-/** This function randomly returns Rock, Paper or Scissors */
+/** This function plays 5 rounds of the game */
+const NUMBER_OF_GAMES = 5;
+const buttons = document.querySelectorAll('button');
+const displayResult = document.getElementById("result");
+const paper = document.querySelector(".paper");
+const rock = document.querySelector(".rock");
+const scissors = document.querySelector(".scissors");
 
+let gameMessage = '';
+let userScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+let gameCount = 1;
+
+displayResult.style.display = "none";
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let playerChoice = btn.textContent.toLowerCase();
+        gameMessage = playRound(playerChoice, getComputerChoice());
+
+        if (playerChoice === 'reset') {
+            reset();
+        } else if (gameCount <= NUMBER_OF_GAMES) {
+            displayResult.style.display = "block";
+
+            if (gameMessage.includes("You lose")) {
+                computerScore++;
+                displayResult.textContent = `User = ${userScore} Draws = ${drawScore} Computer = ${computerScore}\n`;
+            } else if (gameMessage.includes("You Win!")) {
+                userScore++;
+                displayResult.textContent = `User = ${userScore} Draws = ${drawScore} Computer = ${computerScore}\n`;
+            } else if (gameMessage.includes("draw!")) {
+                drawScore++;
+                displayResult.textContent = '\n' + `User = ${userScore} Draws = ${drawScore} Computer = ${computerScore}\n`;
+            }
+            gameCount++;
+        } else {
+            displayResult.innerHTML = displayWinner(userScore, computerScore, drawScore);
+        }
+    });
+});
+
+
+/** This function randomly returns Rock, Paper or Scissors */
 function getComputerChoice() {
     const values = ['Rock', 'Paper', 'Scissors'];
 
@@ -10,22 +53,7 @@ function getComputerChoice() {
     return values[num].toLowerCase();
 }
 
-/** This function gets user selection of Rock, Paper, or Scissors */
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Enter rock, paper, or scissors", "paper");
-
-    while (playerChoice.toLowerCase() !== "rock" &&
-        playerChoice.toLowerCase() !== "paper" &&
-        playerChoice.toLowerCase() !== "scissors"
-    ) {
-        playerChoice = prompt("Enter rock, paper, or scissors", "paper");
-    }
-    return playerChoice.toLowerCase();
-}
-
 /** This function plays a single round of rock, paper, and scissors game */
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It's a draw! You chose ${playerSelection} and computer chose ${computerSelection}.`;
@@ -50,44 +78,27 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-/** This function plays 5 rounds of the game */
-
-function game() {
-
-    const NUMBER_OF_GAMES = 5;
-    let userScore = 0;
-    let computerScore = 0;
-    let drawScore = 0;
-    let gameCount = 1;
-
-    while (gameCount <= NUMBER_OF_GAMES) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-
-        // play game
-        let gameMessage = playRound(playerSelection, computerSelection);
-
-        if (gameMessage.includes("You lose")) {
-            computerScore++;
-        } else if (gameMessage.includes("You Win!")) {
-            userScore++;
-        } else if (gameMessage.includes("draw!")) {
-            drawScore++;
-        }
-        gameCount++;
-    }
-
-    // display winner
-    return displayWinner(userScore, computerScore, drawScore);
-}
-
 /** Function to display game score and winner */
 function displayWinner(userScore, computerScore, drawScore) {
     if (userScore > computerScore) {
-        return `You Win!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}.`;
+        return `You Win!<br>
+        User = ${userScore} Computer = ${computerScore} Draws = ${drawScore}.`;
     } else if (computerScore > userScore) {
-        return `You Lose!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}.`;
+        return `You Lose!<br>
+        User = ${userScore} Computer = ${computerScore} Draws = ${drawScore}.`;
     } else {
-        return `Game is a draw!\nUser = ${userScore}\nComputer = ${computerScore}\nDraws = ${drawScore}`;
+        return `Game is a draw!<br>
+        User = ${userScore} Computer = ${computerScore} Draws = ${drawScore}`;
     }
+}
+
+/** Function to reset game parameters */
+function reset() {
+    displayResult.textContent = "";
+    displayResult.style.display = "none";
+
+    userScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    gameCount = 1;
 }
